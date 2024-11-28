ea35ecae705538db9acbca97452d4eff339576b7
Itachiuchiha786786 <166844972+Itachiuchiha786786@users.noreply.github.com>
2024-11-28 16:12:43 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -3,7 +3,7 @@ class Config(object):
 
     # Get this value from my.telegram.org/apps
     OWNER_ID = "6402009857"
-    sudo_users = "6402009857", "5421067814"
+    sudo_users = "6402009857", "5421067814", "6835013483"
     GROUP_ID = -1002104939708
     TOKEN = "7456960988:AAHzeDfSBi-rS33RZeAZe7b8nvw5SXOLYeM"
     mongo_url = "mongodb+srv://nudewaifubot:nudewaifubot@nude.50fk0.mongodb.net/?retryWrites=true&w=majority&appName=nude"
