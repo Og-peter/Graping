7d0beda48fb5cdb50110d772e68dbb861ad7e8f2
Itachiuchiha786786 <166844972+Itachiuchiha786786@users.noreply.github.com>
2024-12-07 20:50:55 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,13 +2,13 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6402009857"
-    sudo_users = "6402009857", "5421067814", "6835013483"
+    OWNER_ID = "6835013483"
+    sudo_users = "6835013483", "5421067814", "6835013483"
     GROUP_ID = -1002104939708
     TOKEN = "7456960988:AAF2qtGunhDGWzwLRX7Izq4GON_2kS_uxPw"
     mongo_url = "mongodb+srv://nudewaifubot:nudewaifubot@nude.50fk0.mongodb.net/?retryWrites=true&w=majority&appName=nude"
     PHOTO_URL = ["https://ibb.co/q7jzMwz", "https://ibb.co/T2qNx5m", "https://ibb.co/TYS7yC4", "https://ibb.co/hVRR0T1", "https://ibb.co/3W3rVy6"]
-    SUPPORT_CHAT = "dynamic_gangs"
+    SUPPORT_CHAT = "Dyna_community"
     UPDATE_CHAT = "Seizer_updates"
     BOT_USERNAME = "waifu_pro_bot"
     CHARA_CHANNEL_ID = "-1002335814239"
@@ -20,7 +20,7 @@ class Config(object):
     GRADE3 = ["7334126640"]
     GRADE2 = ["6305653111", "5421067814"]
     GRADE1 = ["7004889403", "1374057577", "5158013355", "5630057244", "7334126640"]
-    SPECIALGRADE = ["6402009857","1993290981"]
+    SPECIALGRADE = ["6835013483","1993290981"]
 
     Genin = []
     Chunin = []
